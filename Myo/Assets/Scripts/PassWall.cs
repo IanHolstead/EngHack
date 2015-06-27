53d02298fa94c494323467603bb7b196cdd8225e
Ian Holstead <ian.holstead@gmail.com>
2015-06-27 14:46:48 -0400

    Change wall of looks like crap to wall of looks not bad

--- Myo/Assets/Scripts/PassWall.cs
+++ Myo/Assets/Scripts/PassWall.cs
@@ -5,14 +5,30 @@ public class PassWall : MonoBehaviour {
 
     public float velocity = 0.1f;
     public DustTypes type;
-	
+    public Material cubeMat, sphereMat, triangleMat;
+
     // Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    void Start () {
+        //this.GetComponent<MeshRenderer>().material = findMatterial(type);
+        this.GetComponent<MeshRenderer>().material.SetColor("_MKGlowTexColor", findMatterial(type));
+    }
+
+    public Color findMatterial(DustTypes dustType)
+    {
+        switch (dustType)
+        {
+            case DustTypes.CUBE:
+                return Color.blue;
+            case DustTypes.SPERE:
+                return Color.green;
+            case DustTypes.TRIANGLE:
+                return Color.magenta;
+        }
+        return Color.black;
+    }
+
+    // Update is called once per frame
+        void Update () {
         Vector3 pos = transform.position;
 		pos.z += velocity * Time.deltaTime;;
         transform.position = pos;
