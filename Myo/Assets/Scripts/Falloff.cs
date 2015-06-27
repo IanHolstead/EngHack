846f298143815a6621924f5ae00201c9116cdbbe
Ian Holstead <ian.holstead@gmail.com>
2015-06-27 01:25:35 -0400

    Added falloffScript

---
+++ Myo/Assets/Scripts/Falloff.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Falloff : MonoBehaviour {
+
+    public GameObject PlayerCamera;
+    public float invisibleDistance = 200f;
+    public float visibleDistance = 100f;
+
+	// Use this for initialization
+	void Start () {
+        if (visibleDistance < invisibleDistance)
+        {
+            invisibleDistance = visibleDistance + 100;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Color colour = GetComponent<MeshRenderer>().material.color;
+        colour.a = getTransperency();
+        GetComponent<MeshRenderer>().material.color = colour;
+    }
+
+    private float getTransperency ()
+    {
+        float distanceToCamera = Mathf.Abs(PlayerCamera.transform.position.z - transform.position.z);
+        if (distanceToCamera <= 100f)
+        {
+            return 1f;
+        }
+        else if(distanceToCamera <= 200)
+        {
+            return 1f - ((distanceToCamera - visibleDistance) / 100);
+        }
+
+        return 0f;
+    }
+}
